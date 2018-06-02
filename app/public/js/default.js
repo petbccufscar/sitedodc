a9a1ccbeb5e7f1a1e73766cfeed1cf82e22e1ceb
SampaioFelipe <sampaio.motion@gmail.com>
2018-06-02 10:34:30 -0300

    documentação da funcionalidade stick em default.js

--- app/public/js/default.js
+++ app/public/js/default.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
-  
-	$('[data-toggle="affix"]').each(function() {
 
+  /* Stick */
+  // basta incluir os seguintes atributos na tag html
+  // - data-toggle="affix" -> habilita o stick
+  // - data-parent="?" -> nome do wrapper (opcional)
+  // - data-offset=? -> distancia em pixels do topo (opcional)
+  // - data-stick-class="?" -> classe aplicada quando o stick for aplicado (opcional)
+	$('[data-toggle="affix"]').each(function() {
 		$(this).stick_in_parent({
-			sticky_class: 'affix',
+			sticky_class: $(this).data('stick-class') || 'affix',
 			parent: $(this).data('parent'),
 			offset_top: $(this).data('offset')
 		});
 	});
+
+
 
 });
