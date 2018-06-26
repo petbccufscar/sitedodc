30fba17169398c697a3e36b4d38dfa5ed97c495a
Daniel <daniel.lucio.moura@gmail.com>
2018-06-25 22:54:05 -0300

    adicionado links em alunos_em_acao.html e repositorio atualizado com o novo layout

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 var keystone = require('keystone');
 var middleware = require('./middleware');
+var importRoutes = keystone.importer(__dirname);
 
 // Common Middleware
 keystone.pre('routes', middleware.initLocals);
@@ -7,29 +8,18 @@ keystone.pre('render', middleware.flashMessages);
 
 // Import Route Controllers
 var routes = {
-	views: require('./views'),
-	api: require('./api')
+	views: importRoutes('./views'),
 };
 
 // Setup Route Bindings
 exports = module.exports = function (app) {
-<<<<<<< HEAD
-<<<<<<< HEAD
 	// Views
 	app.get('/', routes.views.index);
 	app.get('/acao', routes.views.alunos_em_acao);
-=======
-	// Home
-	app.get('/', routes.views.home.index);
->>>>>>> fc30542fb9a1a06e05612c2d7ce16da374579d3b
 
-	// Áreas
-	app.get('/area/docente', routes.views.area_docente.index)
-=======
->>>>>>> adbf0f45fdbaed3eaa501903ec8424cc89a6c9ee
+	// // Áreas
+	// app.get('/area/docente', routes.views.area_docente.index)
 
-	app.use('/', routes.views);
-	app.use('/api', routes.api);
 
 	/* Exemplo de páginas com passagem de parâmetros pela url */
 	// app.get('/blog/:category?', routes.views.blog);
